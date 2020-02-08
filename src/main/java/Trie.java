4f226fae6e3cd681afd0a60cf80bc38b572d2bc6
Филипп <filipp.rech11@mail.ru>
2020-02-08 23:40:04 +0300

    Initial commit

---
+++ src/main/java/Trie.java
@@ -0,0 +1,14 @@
+public class Trie {
+    public void trieAdd() {
+
+    }
+    public void trieRemove() {
+
+    }
+    public void trieFind() {
+
+    }
+    public void trieFindAll() {
+
+    }
+}
